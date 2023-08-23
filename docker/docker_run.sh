ed581852717fe1049836a3b9660a66d47edd0ad1
youngjae-avikus <youngjae.you@avikus.ai>
2023-08-23 09:17:56 +0900

    Add docker

---
+++ docker/docker_run.sh
@@ -0,0 +1,18 @@
+
+#!/usr/bin/bash
+
+xhost +
+
+docker run \
+	-it \
+	--rm \
+	--gpus all \
+	--net=host \
+	--shm-size=8g \
+	-e DISPLAY=${DISPLAY} \
+	-v /tmp/argus_socket:/tmp/argus_socket \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+	-v $HOME/workspace/faiss-note:/workspace/ \
+	-w /workspace \
+	--name faiss-container \
+	faiss
